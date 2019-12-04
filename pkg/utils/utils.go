dad9769ea798dc448b5088cacbca4afd3afdb6ec
ueef <artem@arkadev.org>
2019-12-04 16:01:14 +0500

    fix utils/Rotate180

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -25,9 +25,9 @@ func Rotate270(i image.Image) image.Image {
 	})
 	for y := b.Min.Y; y < b.Max.Y; y++ {
 		for x := b.Min.X; x < b.Max.X; x++ {
-			ss := y*src.Stride + x*4
-			ds := (b.Max.X-x-1)*dst.Stride + y*4
-			copy(dst.Pix[ds:ds+4], src.Pix[ss:ss+4])
+			s := y*src.Stride + x*4
+			d := (b.Max.X-x-1)*dst.Stride + y*4
+			copy(dst.Pix[d:d+4], src.Pix[s:s+4])
 		}
 	}
 
@@ -37,15 +37,22 @@ func Rotate270(i image.Image) image.Image {
 func Rotate180(i image.Image) image.Image {
 	src := ConvertToRGBA(i)
 	b := src.Bounds()
-	h := b.Dy()
-	buf := make([]uint8, src.Stride)
-
-	for hs := h / 2; hs > 0; hs-- {
-		ss := (b.Min.Y + hs - 1) * src.Stride
-		ds := (b.Min.Y + h - hs) * src.Stride
-		copy(buf, src.Pix[ds:ds+src.Stride])
-		copy(src.Pix[ds:ds+src.Stride], src.Pix[ss:ss+src.Stride])
-		copy(src.Pix[ss:ss+src.Stride], buf)
+	dst := image.NewRGBA(image.Rectangle{
+		Min: image.Point{
+			X: b.Min.Y,
+			Y: b.Min.X,
+		},
+		Max: image.Point{
+			X: b.Max.Y,
+			Y: b.Max.X,
+		},
+	})
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			s := y*src.Stride + x*4
+			d := (b.Max.X-x-1)*dst.Stride + (b.Max.Y-y-1)*4
+			copy(dst.Pix[d:d+4], src.Pix[s:s+4])
+		}
 	}
 
 	return src
